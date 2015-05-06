a8ac04e83a6216b48aa9be9c4decab6d6b701e12
Scott Burton <scotthburton@gmail.com>
2015-05-06 13:41:52 -0600

    playing with files.

---
+++ files.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Testing Files</title>
+</head>
+
+<body>
+
+<?php
+
+echo "In php...<br />";
+
+$fh = fopen('file.txt', 'a');
+
+fwrite($fh, 'appending to file\n');
+fclose($fh);
+
+echo "File written...<br />";
+
+$fh = fopen('file.txt', 'r');
+
+while ($line = fgets($fh))
+{
+   echo "file content: $line<br />\n";
+}
+
+fclose($fh);
+
+echo "done reading file<br />\n";
+
+?>
+
+
+</body>
+
+</html>
